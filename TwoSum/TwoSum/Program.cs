3532e8d6a33d9b945759c1b3a84b6d26fe23d9b0
Redened <85760145+Redened@users.noreply.github.com>
2025-10-18 00:20:38 +0400

    Upload LeetCode #1: Two Sum

---
+++ TwoSum/TwoSum/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace TwoSum;
+
+public class Program
+{
+    public static void Main()
+    {
+        var solutionResult = Solution.TwoSum([2, 5, 5, 11], 10);
+
+        Console.WriteLine(string.Join(" ", solutionResult));
+    }
+}
+
+public class Solution
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return [i, j];
+                }
+            }
+        }
+
+        return [];
+    }
+}
