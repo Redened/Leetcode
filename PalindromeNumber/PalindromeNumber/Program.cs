b679a7032ea3480cf0625d45beea5eeaab6db286
Redened <85760145+Redened@users.noreply.github.com>
2025-10-18 00:33:29 +0400

    Upload LeetCode #9: Palindrome Number

---
+++ PalindromeNumber/PalindromeNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PalindromeNumber;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Solution.IsPalindrome(155551));
+    }
+}
+
+public class Solution
+{
+    public static bool IsPalindrome(int x)
+    {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        var reversedInputString = new string(x.ToString().Reverse().ToArray());
+
+        if (!int.TryParse(reversedInputString, out int reversedInputNumber)) return false;
+
+        if (x == reversedInputNumber) return true;
+
+        return false;
+    }
+}
