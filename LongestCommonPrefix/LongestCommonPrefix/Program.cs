9c0e82bd6d395a61b4b409013dd11f50e8158653
Redened <85760145+Redened@users.noreply.github.com>
2025-10-18 00:40:32 +0400

    Upload LeetCode #14: Longest Common Prefix

---
+++ LongestCommonPrefix/LongestCommonPrefix/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LongestCommonPrefix;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Solution.LongestCommonPrefix(["flower", "flow", "flight"]));
+    }
+}
+
+public class Solution
+{
+    public static string LongestCommonPrefix(string[] strs)
+    {
+        string commonPrefix = strs[0];
+
+        for (int i = 1; i < strs.Length; i++)
+        {
+            while (!strs[i].StartsWith(commonPrefix))
+            {
+                commonPrefix = commonPrefix.Substring(0, commonPrefix.Length - 1);
+            }
+        }
+
+        return commonPrefix;
+    }
+}
--- README.md
+++ README.md
@@ -21,7 +21,15 @@ An integer is a palindrome when it reads the same backward as forward.
 #### [Solution](PalindromeNumber/PalindromeNumber/Program.cs)
 
 ### LeetCode #13: Roman to Integer (Easy)
+<hr>
 
 Given a roman numeral, convert it to an integer.
 
 #### [Solution](RomanToInteger/RomanToInteger/Program.cs)
+
+### LeetCode #14: Longest Common Prefix (Easy)
+<hr>
+
+Write a function to find the longest common prefix string amongst an array of strings.  
+
+#### [Solution](LongestCommonPrefix/LongestCommonPrefix/Program.cs)
